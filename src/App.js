ee3c7247de650c6c096a70ce21ca2e1ef405b409
matildehomem <matilde.a.s.homem@gmail.com>
2019-02-20 12:50:49 +0000

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Output from './components/Output'
+import Select from './components/controls/Select';
+import Text from './components/controls/Text';
 
+import axios from 'axios'
+
+
+//  
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      paras: 4,
+      format: 'html',
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios.get('https://baconipsum.com/api/?type=all-meat&sentences='+this.state.paras+'&format='+this.state.format)
+      .then((response) => {
+        this.setState({
+          text: response.data,
+        });
+      })
+     
+  }
+  showHtml(x) {
+    this.setState({
+      format: x
+    }, this.getSampleText)
+  }
+  changeParas(number) {
+    this.setState({
+      paras: number
+    }, this.getSampleText)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1>React JS Sample Text Generator</h1>
+        <hr />
+        <form className="form-inline">
+          <div className="form-group">
+            <label> Paragraphs:</label>
+            <Text value={this.state.paras} onChange={this.changeParas.bind(this)}></Text>
+          </div>
+          <div className="form-group">
+            <label> Include HTML:</label>
+            <Select value={this.state.format} onChange={this.showHtml.bind(this)}></Select>
+          </div>
+        </form>
+        <Output value={this.state.text} />
       </div>
     );
   }
